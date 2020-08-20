36a0026892190a6c0ce5cb2124349a626d5f20d3
stanleesocca <nmoreloaded4show@ymail.com>
2020-08-20 16:10:04 +0200

    Modified the visualisation naming scheme.

---
+++ 4.0-Data_Visualisation/initLesson.R
@@ -0,0 +1,17 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+#.get_course_path <- function(){
+#  tryCatch(swirl:::swirl_courses_dir(),
+#           error = function(c) {file.path(find.package("swirl"),"Courses")}
+#  )
+#}
+
+olddir <- getwd()
+assign("olddir", olddir, envir = .GlobalEnv)
+
+setwd(file.path(find.package("swirl"),"Courses", "SummeR", "4.0-Data_Visualization"))
+reeffish <- read.csv("ReefFish.csv")
+#reeffish<- read.csv(file.path(.get_course_path(), "SummeR", "4.0-Data_Visualisation", "ReefFish.csv"))
